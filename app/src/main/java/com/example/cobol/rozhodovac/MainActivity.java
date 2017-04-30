8a3c3ea86204adf93dd0d24620a1dfb27a6ea6bd
Cobol01 <marekcarbol@gmail.com>
2017-04-30 22:00:00 +0200

    Poznámkovník předělán do databáze

--- app/src/main/java/com/example/cobol/rozhodovac/MainActivity.java
+++ app/src/main/java/com/example/cobol/rozhodovac/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.cobol.rozhodovac;
 
 import android.content.Intent;
 import android.database.Cursor;
+import android.database.DatabaseUtils;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -16,27 +18,25 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Pole pro výpis do ViewList
     static ArrayList<String> notes = new ArrayList<>();
+
+    // Pole pro předání pozice v tabulce
+    static ArrayList<Integer> poradi = new ArrayList<>();
+
+    // Adaptér pro řizení ViewListu
     static ArrayAdapter arrayAdapter;
 
     static SQLiteDatabase databazeRozhodnuti;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.add_note_menu, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
+    // Funkce pro otevření položky v nové aktivitě určené pro úpravy
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
-
-        //super.onOptionsItemSelected(item);
-
+        super.onOptionsItemSelected(item);
 
         if(item.getItemId() == R.id.add_note){
 
@@ -54,32 +54,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Načtení nebo vytvoření databáze
         databazeRozhodnuti = this.openOrCreateDatabase("Rozhodnuti", MODE_PRIVATE, null);
 
-        //databazeRozhodnuti.execSQL("DROP TABLE rozhodnuti ");
+        //tady se to bude volat
+        nacteniDatabaze();
 
-        /*databazeRozhodnuti.execSQL("CREATE TABLE IF NOT EXISTS rozhodnuti (nazev VARCHAR, poradi INT(3))");
-        databazeRozhodnuti.execSQL("INSERT INTO rozhodnuti (nazev, poradi) VALUES ('Prvni', 1)");
-        databazeRozhodnuti.execSQL("INSERT INTO rozhodnuti (nazev, poradi) VALUES ('Druhy', 2)");*/
-
-        Cursor c = databazeRozhodnuti.rawQuery("SELECT * FROM rozhodnuti", null);
-
-        int nazevIndex = c.getColumnIndex("nazev");
-        int poradiIndex = c.getColumnIndex("poradi");
-
-        c.moveToFirst();
-
-
-        while (c != null){
-            notes.add(c.getString(nazevIndex));
-            if(!c.isLast()) c.moveToNext();
-            else c = null;
-        }
-
+        Toast.makeText(getApplicationContext(), "Načteno", Toast.LENGTH_SHORT).show();
 
         ListView listView = (ListView) findViewById(R.id.listView);
-
-        //notes.add("A další");
 
         arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, notes);
 
@@ -87,11 +70,15 @@ public class MainActivity extends AppCompatActivity {
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            public void onItemClick (AdapterView<?> parent, View view, int position, long id) {
+
+                Log.i("muj_position: ",String.valueOf(position));
 
                 Intent intent = new Intent (getApplicationContext(), NoteEditorActivity.class);
 
-                intent.putExtra("noteId", position);
+                intent.putExtra("noteId", poradi.get(position));
+
+                //Log.i("arr: ", Arrays.toString(poradi.toArray()));
 
                 startActivity(intent);
 
@@ -103,10 +90,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
 
-                notes.remove(position);
+                // Smaže záznam
+                databazeRozhodnuti.execSQL("DELETE FROM rozhodnuti WHERE rowid = " + poradi.get(position));
+
+                // Načte databázi a zaktualizuje podle ní ListView
+                nacteniDatabaze();
                 arrayAdapter.notifyDataSetChanged();
 
-                return false;
+                // Musí vracet true, jinak se aktivuje i ClickListener
+                return true;
             }
         });
 
@@ -119,6 +111,45 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
 
+    // Nachystané menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_note_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public static void nacteniDatabaze(){
+
+        // Načtení tabulky "rozhodnuti" včetně rowid
+        Cursor c = databazeRozhodnuti.rawQuery("SELECT rowid, * FROM rozhodnuti", null);
+
+        // Nastavení indexů
+        int rowidIndex = c.getColumnIndex("rowid");
+        int nazevIndex = c.getColumnIndex("nazev");
+        int poradiIndex = c.getColumnIndex("poradi");
+
+        // Posunutí ukazatele na první položku
+        c.moveToFirst();
+
+        // Pročištění polí před naplněním
+        notes.clear();
+        poradi.clear();
+
+        while (c != null){
+            // Ukládá číslo záznamu a jméno do notes
+            notes.add(c.getInt(rowidIndex) + ": " + c.getString(nazevIndex));
+
+            // Ukládá číslo záznamu do poradi
+            poradi.add(c.getInt(rowidIndex));
+
+            // Kontroluje, zda již nejsme na konci
+            if (c.moveToNext() == false) break;
+
+            //if(!c.isLast()) c.moveToNext();
+            //else c = null;
+        }
     }
 }
--- app/src/main/java/com/example/cobol/rozhodovac/NoteEditorActivity.java
+++ app/src/main/java/com/example/cobol/rozhodovac/NoteEditorActivity.java
@@ -1,35 +1,54 @@
 package com.example.cobol.rozhodovac;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.DatabaseUtils;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class NoteEditorActivity extends AppCompatActivity {
+
     int noteId;
+    EditText editText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_editor);
 
-        EditText editText = (EditText) findViewById(R.id.editText);
+        Button ulozButton = (Button) findViewById(R.id.ulozitButton);
+        Button zrusButton = (Button) findViewById(R.id.zrusitButton);
+
+        editText = (EditText) findViewById(R.id.editText);
 
         Intent intent = getIntent();
 
         noteId = intent.getIntExtra("noteId", -1);
 
-        if (noteId != -1){
-            editText.setText(MainActivity.notes.get(noteId));
-        } else {
-            MainActivity.notes.add("");
-            noteId = MainActivity.notes.size() - 1;
-            MainActivity.arrayAdapter.notifyDataSetChanged();
-        }
+        Log.i("muj_noteId: ",String.valueOf(noteId));
 
+        if (noteId != -1){
+            //editText.setText(MainActivity.notes.get(noteId));
+            //noteId ++;
+            Cursor c = MainActivity.databazeRozhodnuti.rawQuery("SELECT rowid, nazev FROM rozhodnuti WHERE rowid = " + noteId, null);
+            c.moveToFirst();
+            editText.setText(c.getString(c.getColumnIndex("nazev")));
+        } else {
+            // PŘEKOPAT DO DATABÁZE
+            /*MainActivity.notes.add("");
+            noteId = MainActivity.notes.size() - 1;
+            MainActivity.arrayAdapter.notifyDataSetChanged();*/
+
+
+        }
 
         editText.addTextChangedListener(new TextWatcher() {
             @Override
@@ -39,9 +58,10 @@ public class NoteEditorActivity extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-
+                /*
                 MainActivity.notes.set(noteId,String.valueOf(s));
                 MainActivity.arrayAdapter.notifyDataSetChanged();
+                */
 
             }
 
@@ -50,12 +70,30 @@ public class NoteEditorActivity extends AppCompatActivity {
 
             }
         });
+
+        ulozButton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (editText.getText().length() != 0) {
+                    if (noteId != -1){
+                        MainActivity.databazeRozhodnuti.execSQL("UPDATE rozhodnuti SET nazev = \"" + editText.getText() + "\" WHERE rowid = " + noteId);
+                    } else {
+                        MainActivity.databazeRozhodnuti.execSQL("INSERT INTO rozhodnuti (nazev, poradi) VALUES ('" + editText.getText() + "', 1)");
+                    }
+                    //MainActivity.arrayAdapter.notifyDataSetChanged();
+                    Toast.makeText(getApplicationContext(), "Uloženo", Toast.LENGTH_SHORT).show();
+                }
+                MainActivity.nacteniDatabaze();
+                MainActivity.arrayAdapter.notifyDataSetChanged();
+                finish();
+            }
+        });
+
     }
+
 
     @Override
     protected void onStop() {
-
-        Toast.makeText(getApplicationContext(), "konéc", Toast.LENGTH_SHORT).show();
 
         super.onStop();
         getDelegate().onStop();
